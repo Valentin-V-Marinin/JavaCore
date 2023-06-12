c2839c285b24d7a5a4b40168594ae7cec4dd914c
Valentin-V-Marinin <v_v_m_70@mail.ru>
2023-06-12 16:09:44 +0700

    JavaCore Seminar_1 HomeWork

---
+++ seminar1/Main.java
@@ -0,0 +1,8 @@
+package seminar1;
+
+public class Main {
+//    public static void main(String[] args) {
+//        TimePrinter timePrinter = new TimePrinter(new TimeCounter(new DataReciever().getDate()));
+//        timePrinter.print();
+//    }
+}
